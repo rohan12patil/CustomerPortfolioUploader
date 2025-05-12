60babd99298c1e4cd6389d5336db3b31f445889e
Rohan Patil <rohan12patil@gmail.com>
2025-05-12 10:33:01 +0100

    Dark Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import * as CustomerActions from './store/customer.actions';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { TableComponent } from './components/table/table.component';
@@ -8,16 +10,28 @@ import { TableComponent } from './components/table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FileUploadComponent, TableComponent, MatToolbarModule],
+  imports: [
+    FileUploadComponent,
+    TableComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss', 
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store) {}
   title = 'Customer Portfolio Uploader';
+  isDarkMode = false;
 
   ngOnInit() {
-    // Try to restore any saved state when the app starts
+    // Restore any saved state when the app starts
     this.store.dispatch(CustomerActions.restoreCustomers());
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme');
   }
 }
